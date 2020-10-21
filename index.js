a92623f93fdc9ea537575c91ff4b351a623d81e9
khalil zaarour <k.zaarour1997@gmail.com>
2020-10-21 22:54:45 +0300

    Step5

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 let date = new Date();
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
 app.get("/", (req, res) => res.send("ok"));
 app.get("/test", (req, res) => res.send({status:200, message:"ok"}));
 app.get("/time", (req, res) => res.send({status:200, message:date.getHours() +":"+date.getMinutes()}));
-app.get('/hello/:id', (req, res)=> {
+app.get('/hello/:id?', (req, res)=> {
     if(Boolean(req.params.id)=== true){
         res.send({status:200, message:"Hello,"+req.params.id});
 
@@ -16,12 +22,12 @@ app.get('/hello/:id', (req, res)=> {
     if (req.params.s) {
       res.send({ status: 200, message: `ok`, data: req.params.s});
     } else {
-      res.send({
-        status: 500,
-        error: true,
-        message: "you have to provide a search"
-      });
+      res.send({status: 500,error: true,message: "you have to provide a search"});
     }
   });
-
+  app.get('/movies/read', (req, res) => 
+    res.send({status:200, data:movies}));
+  app.get("/movies/create", (req, res) => res.send("create"));
+  app.get("/movies/update", (req, res) => res.send("update"));
+  app.get("/movies/delete", (req, res) => res.send("delete"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
