1cc279a8e2ce13f7c2ac5aeacefd971b9bf4b4b2
khalil zaarour <k.zaarour1997@gmail.com>
2020-10-21 20:00:48 +0300

    Step3

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+let date = new Date();
 app.get("/", (req, res) => res.send("ok"));
+app.get("/test", (req, res) => res.send({status:200, message:"ok"}));
+app.get("/time", (req, res) => res.send({status:200, message:date.getHours() +":"+date.getMinutes()}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
